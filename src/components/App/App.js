f4d5a5ce7ef95863603f820f37997825b111b229
williams <ikswaqqs40@gmail.com>
2022-06-16 23:42:12 +0100

    made app responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import './App.css';
-import SideMenu from "../SideMenu";
-import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import React from "react";
+import "./App.css";
+import {
+  makeStyles,
+  CssBaseline,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
 import Header from "../Header";
-import PageHeader from '../PageHeader';
 
 import Personnels from "../../pages/Personnel/Personnels";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: "#333996",
-      light: '#3c44b126'
+      light: "#3c44b126",
     },
     secondary: {
       main: "#f83245",
-      light: '#f8324526'
+      light: "#f8324526",
     },
     background: {
-      default: "#f4f5fd"
+      default: "#f4f5fd",
     },
   },
-  overrides:{
-    MuiAppBar:{
-      root:{
-        transform:'translateZ(0)'
-      }
-    }
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: "translateZ(0)",
+      },
+    },
   },
-  props:{
-    MuiIconButton:{
-      disableRipple:true
-    }
-  }
-})
-
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
-    paddingLeft: '320px',
-    width: '100%'
-  }
-})
+    width: "100%",
+  },
+});
 
 function App() {
   const classes = useStyles();
 
   return (
     <ThemeProvider theme={theme}>
-      <SideMenu />
       <div className={classes.appMain}>
         <Header />
-        
+
         <Personnels />
       </div>
       <CssBaseline />
--- src/pages/Personnel/Personnels.js
+++ src/pages/Personnel/Personnels.js
@@ -1,187 +1,210 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import PersonnelForm from "./PersonnelForm";
 import PageHeader from "../../components/PageHeader";
-import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
-import { Paper, makeStyles, TableBody, TableRow, TableCell, Toolbar, InputAdornment } from '@material-ui/core';
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
+import {
+  Paper,
+  makeStyles,
+  TableBody,
+  TableRow,
+  TableCell,
+  Toolbar,
+  InputAdornment,
+} from "@material-ui/core";
 import useTable from "../../components/useTable";
 import * as personnelService from "../../services/personnelService";
 import Controls from "../../components/controls/Controls";
 import { Search } from "@material-ui/icons";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 import Popup from "../../components/Popup";
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import CloseIcon from '@material-ui/icons/Close';
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import CloseIcon from "@material-ui/icons/Close";
 import Notification from "../../components/Notification";
 import ConfirmDialog from "../../components/ConfirmDialog";
 
-const useStyles = makeStyles(theme => ({
-    pageContent: {
-        margin: theme.spacing(5),
-        padding: theme.spacing(3)
+const useStyles = makeStyles((theme) => ({
+  pageContent: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+    overflowX: "auto",
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginTop: theme.spacing(6.25),
+  },
+  searchInput: {
+    width: "75%",
+    [theme.breakpoints.down("xs")]: {
+      width: "65%",
     },
-    searchInput: {
-        width: '75%'
-    },
-    newButton: {
-        position: 'absolute',
-        right: '10px'
-    }
-}))
-
+  },
+  newButton: {
+    position: "absolute",
+    right: "10px",
+  },
+}));
 
 const headCells = [
-    { id: 'fullName', label: 'Personnel Name' },
-    { id: 'email', label: 'Email Address (Personal)' },
-    { id: 'mobile', label: 'Mobile Number' },
-    { id: 'department', label: 'Department' },
-    { id: 'actions', label: 'Actions', disableSorting: true }
-]
+  { id: "fullName", label: "Personnel Name" },
+  { id: "email", label: "Email Address (Personal)" },
+  { id: "mobile", label: "Mobile Number" },
+  { id: "department", label: "Department" },
+  { id: "actions", label: "Actions", disableSorting: true },
+];
 
 export default function Personnels() {
+  const classes = useStyles();
+  const [recordForEdit, setRecordForEdit] = useState(null);
+  const [records, setRecords] = useState(personnelService.getAllPersonnels());
+  const [filterFn, setFilterFn] = useState({
+    fn: (items) => {
+      return items;
+    },
+  });
+  const [openPopup, setOpenPopup] = useState(false);
+  const [notify, setNotify] = useState({
+    isOpen: false,
+    message: "",
+    type: "",
+  });
+  const [confirmDialog, setConfirmDialog] = useState({
+    isOpen: false,
+    title: "",
+    subTitle: "",
+  });
 
-    const classes = useStyles();
-    const [recordForEdit, setRecordForEdit] = useState(null)
-    const [records, setRecords] = useState(personnelService.getAllPersonnels())
-    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
-    const [openPopup, setOpenPopup] = useState(false)
-    const [notify, setNotify] = useState({ isOpen: false, message: '', type: '' })
-    const [confirmDialog, setConfirmDialog] = useState({ isOpen: false, title: '', subTitle: '' })
+  const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } =
+    useTable(records, headCells, filterFn);
 
-    const {
-        TblContainer,
-        TblHead,
-        TblPagination,
-        recordsAfterPagingAndSorting
-    } = useTable(records, headCells, filterFn);
-
-    const handleSearch = e => {
-        let target = e.target;
-        setFilterFn({
-            fn: items => {
-                if (target.value === "")
-                    return items;
-                else
-                    return items.filter(x => x.fullName.toLowerCase().includes(target.value))
-            }
-        })
-    }
-
-    const addOrEdit = (Personnel, resetForm) => {
-        if (Personnel.id === 0)
-            personnelService.insertPersonnel(Personnel)
+  const handleSearch = (e) => {
+    let target = e.target;
+    setFilterFn({
+      fn: (items) => {
+        if (target.value === "") return items;
         else
-            personnelService.updatePersonnel(Personnel)
-        resetForm()
-        setRecordForEdit(null)
-        setOpenPopup(false)
-        setRecords(personnelService.getAllPersonnels())
-        setNotify({
-            isOpen: true,
-            message: 'Submitted Successfully',
-            type: 'success'
-        })
-    }
+          return items.filter((x) =>
+            x.fullName.toLowerCase().includes(target.value)
+          );
+      },
+    });
+  };
 
-    const openInPopup = item => {
-        setRecordForEdit(item)
-        setOpenPopup(true)
-    }
+  const addOrEdit = (Personnel, resetForm) => {
+    if (Personnel.id === 0) personnelService.insertPersonnel(Personnel);
+    else personnelService.updatePersonnel(Personnel);
+    resetForm();
+    setRecordForEdit(null);
+    setOpenPopup(false);
+    setRecords(personnelService.getAllPersonnels());
+    setNotify({
+      isOpen: true,
+      message: "Submitted Successfully",
+      type: "success",
+    });
+  };
 
-    const onDelete = id => {
-        setConfirmDialog({
-            ...confirmDialog,
-            isOpen: false
-        })
-        personnelService.deletePersonnel(id);
-        setRecords(personnelService.getAllPersonnels())
-        setNotify({
-            isOpen: true,
-            message: 'Deleted Successfully',
-            type: 'error'
-        })
-    }
+  const openInPopup = (item) => {
+    setRecordForEdit(item);
+    setOpenPopup(true);
+  };
 
-    return (
-        <>
-            <PageHeader
-                title="Personnel Manager v1.2.11"
-                subTitle="Manage all personnel records"
-                icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
-            />
-            <Paper className={classes.pageContent}>
+  const onDelete = (id) => {
+    setConfirmDialog({
+      ...confirmDialog,
+      isOpen: false,
+    });
+    personnelService.deletePersonnel(id);
+    setRecords(personnelService.getAllPersonnels());
+    setNotify({
+      isOpen: true,
+      message: "Deleted Successfully",
+      type: "error",
+    });
+  };
 
-                <Toolbar>
-                    <Controls.Input
-                        label="Search Personnels"
-                        className={classes.searchInput}
-                        InputProps={{
-                            startAdornment: (<InputAdornment position="start">
-                                <Search />
-                            </InputAdornment>)
-                        }}
-                        onChange={handleSearch}
-                    />
-                    <Controls.Button
-                        text="Add New"
-                        variant="outlined"
-                        startIcon={<AddIcon />}
-                        className={classes.newButton}
-                        onClick={() => { setOpenPopup(true); setRecordForEdit(null); }}
-                    />
-                </Toolbar>
-                <TblContainer>
-                    <TblHead />
-                    <TableBody>
-                        {
-                            recordsAfterPagingAndSorting().map(item =>
-                                (<TableRow key={item.id}>
-                                    <TableCell>{item.fullName}</TableCell>
-                                    <TableCell>{item.email}</TableCell>
-                                    <TableCell>{item.mobile}</TableCell>
-                                    <TableCell>{item.department}</TableCell>
-                                    <TableCell>
-                                        <Controls.ActionButton
-                                            color="primary"
-                                            onClick={() => { openInPopup(item) }}>
-                                            <EditOutlinedIcon fontSize="small" />
-                                        </Controls.ActionButton>
-                                        <Controls.ActionButton
-                                            color="secondary"
-                                            onClick={() => {
-                                                setConfirmDialog({
-                                                    isOpen: true,
-                                                    title: 'Are you sure to delete this record?',
-                                                    subTitle: "You can't undo this operation",
-                                                    onConfirm: () => { onDelete(item.id) }
-                                                })
-                                            }}>
-                                            <CloseIcon fontSize="small" />
-                                        </Controls.ActionButton>
-                                    </TableCell>
-                                </TableRow>)
-                            )
-                        }
-                    </TableBody>
-                </TblContainer>
-                <TblPagination />
-            </Paper>
-            <Popup
-                title="Personnel Form"
-                openPopup={openPopup}
-                setOpenPopup={setOpenPopup}
-            >
-                <PersonnelForm
-                    recordForEdit={recordForEdit}
-                    addOrEdit={addOrEdit} />
-            </Popup>
-            <Notification
-                notify={notify}
-                setNotify={setNotify}
-            />
-            <ConfirmDialog
-                confirmDialog={confirmDialog}
-                setConfirmDialog={setConfirmDialog}
-            />
-        </>
-    )
+  return (
+    <>
+      <PageHeader
+        title="Personnel Manager v1.2.11"
+        subTitle="Manage all personnel records"
+        icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+      />
+      <Paper className={classes.pageContent}>
+        <Toolbar className={classes.toolbar}>
+          <Controls.Input
+            label="Search Personnels"
+            className={classes.searchInput}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handleSearch}
+          />
+          <Controls.Button
+            text="Add New"
+            variant="outlined"
+            startIcon={<AddIcon />}
+            className={classes.newButton}
+            onClick={() => {
+              setOpenPopup(true);
+              setRecordForEdit(null);
+            }}
+          />
+        </Toolbar>
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {recordsAfterPagingAndSorting().map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+                <TableCell>
+                  <Controls.ActionButton
+                    color="primary"
+                    onClick={() => {
+                      openInPopup(item);
+                    }}
+                  >
+                    <EditOutlinedIcon fontSize="small" />
+                  </Controls.ActionButton>
+                  <Controls.ActionButton
+                    color="secondary"
+                    onClick={() => {
+                      setConfirmDialog({
+                        isOpen: true,
+                        title: "Are you sure to delete this record?",
+                        subTitle: "You can't undo this operation",
+                        onConfirm: () => {
+                          onDelete(item.id);
+                        },
+                      });
+                    }}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </Controls.ActionButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+        <TblPagination />
+      </Paper>
+      <Popup
+        title="Personnel Form"
+        openPopup={openPopup}
+        setOpenPopup={setOpenPopup}
+      >
+        <PersonnelForm recordForEdit={recordForEdit} addOrEdit={addOrEdit} />
+      </Popup>
+      <Notification notify={notify} setNotify={setNotify} />
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
+    </>
+  );
 }
